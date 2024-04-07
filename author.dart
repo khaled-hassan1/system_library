fbe6154e1398b0661ed4d64c372e674b0c03554d
Our-Org2024 <organization.303@gmail.com>
2024-04-07 23:15:29 +0200

    first

---
+++ author.dart
@@ -0,0 +1,10 @@
+class ListBooks {}
+
+class Author {
+  final String name, nationality;
+  final int birth;
+  final ListBooks books;
+
+  Author(this.name, this.nationality, this.birth, this.books);
+
+}
---
+++ lib.dart
@@ -0,0 +1,8 @@
+import 'book.dart';
+
+class Library {
+  void addBook(){}
+  void removeBook(){}
+  void searchingBook(Book book){}
+  void dipBookAvailableBooks(Book book){}
+}
